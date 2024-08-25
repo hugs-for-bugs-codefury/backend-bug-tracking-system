7fcba6157e0bdac14ce45656ab6f240688161e70
Anantashayana <anantashayana.hegde@gmail.com>
2024-08-25 10:04:57 +0530

    added javadoc to services

--- src/main/java/org/example/services/IDeveloperService.java
+++ src/main/java/org/example/services/IDeveloperService.java
@@ -4,13 +4,73 @@ import org.example.models.*;
 
 import java.util.List;
 
-public interface IDeveloperService extends IUserService{
+/**
+ * The IDeveloperService interface provides methods for managing developer-specific operations,
+ * including user registration, login, and handling of assigned projects and bugs.
+ * It extends the IUserService interface, inheriting its user-related functionalities.
+ */
+public interface IDeveloperService extends IUserService {
+
+    /**
+     * Registers a new developer with the given details.
+     *
+     * @param name     the name of the developer.
+     * @param email    the email address of the developer.
+     * @param password the password for the developer's account.
+     * @return the registered Developer object.
+     */
     public Developer registerUser(String name, String email, String password);
+
+    /**
+     * Logs in a developer using the provided email and password.
+     *
+     * @param email    the email address of the developer.
+     * @param password the password for the developer's account.
+     * @return the Developer object if login is successful.
+     */
     public Developer login(String email, String password);
+
+    /**
+     * Retrieves the currently logged-in developer.
+     *
+     * @return the Developer object representing the currently logged-in developer.
+     */
     public Developer getCurrentDeveloper();
+
+    /**
+     * Retrieves a developer by their unique developer ID.
+     *
+     * @param developerId the unique identifier of the developer.
+     * @return the Developer object associated with the provided developer ID.
+     */
     public Developer getDeveloper(int developerId);
+
+    /**
+     * Closes a bug by its unique bug ID.
+     *
+     * @param bugId the unique identifier of the bug to be closed.
+     */
     public void closeBug(int bugId);
+
+    /**
+     * Retrieves the project assigned to the currently logged-in developer.
+     *
+     * @return the Project object representing the assigned project.
+     */
     public Project getAssignedProject();
+
+    /**
+     * Retrieves the project assigned to a specific developer by their unique developer ID.
+     *
+     * @param developerId the unique identifier of the developer.
+     * @return the Project object representing the assigned project for the specified developer.
+     */
     public Project getAssignedProject(int developerId);
+
+    /**
+     * Retrieves the list of bugs assigned to the currently logged-in developer.
+     *
+     * @return a list of Bug objects representing the assigned bugs.
+     */
     public List<Bug> getAssignedBugs();
 }
--- src/main/java/org/example/services/IProjectManagerService.java
+++ src/main/java/org/example/services/IProjectManagerService.java
@@ -1,17 +1,90 @@
 package org.example.services;
 
 import org.example.models.*;
-
 import java.time.LocalDateTime;
 
+/**
+ * The IProjectManagerService interface extends the IUserService interface
+ * and defines additional methods specifically for project manager-related
+ * functionalities within the bug tracking system.
+ *
+ * This interface provides methods for project manager registration, login,
+ * retrieving project manager information, managing projects, and assigning
+ * team members (testers and developers) to projects and bugs. It is intended
+ * to be implemented by a service class that handles the business logic
+ * associated with project managers.
+ */
 public interface IProjectManagerService extends IUserService {
+
+    /**
+     * Registers a new project manager with the given details.
+     *
+     * @param name     The name of the project manager.
+     * @param email    The email address of the project manager.
+     * @param password The password for the project manager's account.
+     * @return The newly registered ProjectManager object.
+     *
+
+     */
     public ProjectManager registerUser(String name, String email, String password);
+
+    /**
+     * Authenticates a project manager with the provided email and password.
+     *
+     * @param email    The email address of the project manager trying to log in.
+     * @param password The password corresponding to the email.
+     * @return The ProjectManager object if login is successful; otherwise, null.
+     *
+
+     */
     public ProjectManager login(String email, String password);
+
+    /**
+     * Retrieves the currently logged-in project manager.
+     *
+     * @return The ProjectManager object representing the currently authenticated project manager.
+     *
+
+     */
     public ProjectManager getCurrentProjectManager();
+
+    /**
+     * Retrieves a project manager by their unique project manager ID.
+     *
+     * @param projectManagerId The unique ID of the project manager.
+     * @return The ProjectManager object corresponding to the given projectManagerId.
+     *
+
+     */
     public ProjectManager getProjectManager(int projectManagerId);
+
+    /**
+     * Creates a new project with the specified name and start date.
+     *
+     * @param name      The name of the project to be created.
+     * @param startDate The start date of the project.
+     * @return The newly created Project object.
+     *
+
     Project createProject(String name, LocalDateTime startDate);
+
+    /**
+     * Assigns a tester to a project.
+     *
+     * @param project The Project object to which the tester will be assigned.
+     * @param tester  The Tester object representing the tester to be assigned.
+     *
+
+     */
     public void assignTester(Project project, Tester tester);
+
+    /**
+     * Assigns a developer to a bug for resolution.
+     *
+     * @param bug       The Bug object representing the bug to be assigned.
+     * @param developer The Developer object representing the developer who will resolve the bug.
+     *
+
+     */
     public void assignDeveloper(Bug bug, Developer developer);
-
-
 }
