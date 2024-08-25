881fcd7cbc4821db0462e6950c91e109b3b705a6
shreya-gp <109740550+shreya-gp@users.noreply.github.com>
2024-08-25 20:20:39 +0530

    fix bug

--- src/main/java/org/example/exceptions/projects/MaxProjectsException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exceptions.projects;
-
-public class MaxProjectsException extends Exception {
-    public MaxProjectsException(String message) {
-        super(message);
-    }
-}
